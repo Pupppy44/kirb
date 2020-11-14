4631f153d6587e4a139d922120efa9c6ab483869
Pupppy44 <42492375+Pupppy44@users.noreply.github.com>
2020-11-14 14:46:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-// server.js
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.send("It's a secret to everyone");
+});
+
+app.listen(process.env.PORT);
